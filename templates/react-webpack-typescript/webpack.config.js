925ea6a1cff131c002b59ff897b0c83b3d9b9b16
EdwardHoward <edwardhoward11@gmail.com>
2021-09-14 20:29:39 -0600

    Add lint to react-webpack template

--- templates/react-webpack-typescript/webpack.config.js
+++ templates/react-webpack-typescript/webpack.config.js
@@ -2,32 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-          test:/\.(s*)css$/,
-          use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js', '.scss', '.css' ]
-  },
-  plugins: [
-   new HtmlWebpackPlugin({
-      template: 'index.html',
-      inject: true
-   })
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    mode: 'production',
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(s*)css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.scss', '.css'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            inject: true,
+        }),
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 };
